cab4392c1b31fa5dc6626cb6308805cb467ddce2
Gazdag András <gazdag.andras@gmail.com>
2015-04-10 16:13:37 +0200

    Rács a rácsban, képek

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="stylesheet" href="css/base.css" type="text/css">
-        
+
     </head>
     <body>
 
@@ -37,14 +37,35 @@
             </div>
 
             <div class="row">
-                <div class="col-md-8" id="content">
+                <div class="col-md-8 col-sm-7 col-xs-12" id="content">
                     Tartalom  
+
+                    <h3>Képek:</h3>
+
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <img src="images/Minion_dave.jpg" class="mini">
+                        </div>
+                        <div class="col-sm-4">
+                            <img src="images/Minion_dave.jpg" class="mini">
+                        </div>
+                        <div class="col-sm-4">
+                            <img src="images/Minion_dave.jpg" class="mini">
+                        </div>
+                        <div class="col-sm-4">
+                            <img src="images/Minion_dave.jpg" class="mini">
+                        </div>
+                        <div class="col-sm-4">
+                            <img src="images/Minion_dave.jpg" class="mini">
+                        </div>
+                    </div>
+
                 </div>
-                <div class="col-md-4" id="sidebar">
+                <div class="col-md-4 col-sm-5 col-xs-12" id="sidebar">
                     Oldalsáv  
                 </div>
             </div>
-            
+
             <div class="row">
                 <div class="col-md-12" id="footer">
                     Lábléc    
