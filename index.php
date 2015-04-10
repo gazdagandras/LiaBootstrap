d1f93ea70e0169700e81caf86c825ecdc2d3b441
Gazdag András <gazdag.andras@gmail.com>
2015-04-10 15:10:59 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Boostrap próbaoldal</title>
+
+        <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    </head>
+    <body>
+
+        <div class="container">
+
+            <div class="row">
+                <div class="col-md-12" id="header">
+                    <h1>Boostrap próbaoldal</h1>    
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12" id="navigation">
+                    Navigáció    
+                </div>
+            </div>
+
+
+        </div>
+    </body>
+</html>
