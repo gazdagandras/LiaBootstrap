a4dcefc9056cad497ba67deeb30a0f27d1b1a983
Gazdag András <gazdag.andras@gmail.com>
2015-04-10 15:56:33 +0200

    Színek

--- index.php
+++ index.php
@@ -17,6 +17,8 @@
 
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="stylesheet" href="css/base.css" type="text/css">
+        
     </head>
     <body>
 
