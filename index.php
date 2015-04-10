3c60cad8d4e22d0c4c1de0027289431fc9075ac3
Gazdag András <gazdag.andras@gmail.com>
2015-04-10 17:29:45 +0200

    Slideshow

--- index.php
+++ index.php
@@ -78,6 +78,48 @@
                     <p>
                         <a class="btn btn-primary btn-xs" href="http://google.com">Google</a>
 
+                    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                            <div class="item active">
+                                <img src="images/kep1.jpeg" alt="Kép 1">
+                                <div class="carousel-caption">
+                                    Első
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img src="images/kep2.jpeg" alt="Kép 2">
+                                <div class="carousel-caption">
+                                    Második
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img src="images/kep3.jpeg" alt="Kép 3">
+                                <div class="carousel-caption">
+                                    Harmadik
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Controls -->
+                        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+
+
                 </div>
                 <div class="col-md-4 col-sm-5 col-xs-12" id="sidebar">
                     Oldalsáv  
